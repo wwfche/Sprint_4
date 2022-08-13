cc725a61f390a9e0f6503f1780f85d0bde609a0c
wwfche <khutsiev.d@gmail.com>
2022-08-13 23:29:08 +0300

    add HomePageTest

---
+++ src/main/java/ru/yandex/practicum/HomePage.java
@@ -0,0 +1,100 @@
+package ru.yandex.practicum;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.After;
+import org.junit.Test;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+
+
+
+public class HomePage {
+    private WebDriver driver;
+    private By price = By.id("accordion__heading-0");
+    private By priceText = By.cssSelector(".accordion__item:nth-child(1) p");
+    private By many = By.id("accordion__heading-1");
+    private By manyText = By.cssSelector(".accordion__item:nth-child(2) p");
+    private By time = By.id("accordion__heading-2");
+    private By timeText = By.cssSelector(".accordion__item:nth-child(3) p");
+    private By today = By.id("accordion__heading-3");
+    private By todayText = By.cssSelector(".accordion__item:nth-child(4) p");
+    private By extend = By.id("accordion__heading-4");
+    private By extendText = By.cssSelector(".accordion__item:nth-child(5) p");
+    private By charger = By.id("accordion__heading-5");
+    private By chargerText = By.cssSelector(".accordion__item:nth-child(6) p");
+    private By cancel = By.id("accordion__heading-6");
+    private By cancelText = By.cssSelector(".accordion__item:nth-child(7) p");
+    private By mkad = By.id("accordion__heading-7");
+    private By mkadText = By.cssSelector(".accordion__item:nth-child(8) p");
+
+    public By hueta = By.cssSelector("#rcc-confirm-button");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void waitForLoadHomePage() {
+        new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className("Home_SubHeader__zwi_E")));
+    }
+
+    public void clickHueta(){
+        driver.findElement(hueta).click();
+    }
+    public void clickPrice() {
+
+//        driver.findElement(price);
+//        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", price);
+        driver.findElement(price).click();
+    }
+    public String getPriceText() {
+        return driver.findElement(priceText).getText();
+
+    }
+    public void clickMany() {
+        driver.findElement(many).click();
+    }
+    public String getManyText() {
+        return driver.findElement(manyText).getText();
+    }
+    public void clickTime(){
+        driver.findElement(time).click();
+    }
+    public String getTimeText() {
+        return driver.findElement(timeText).getText();
+    }
+    public void clickToday(){
+        driver.findElement(today).click();
+    }
+    public String getTodayText(){
+        return driver.findElement(todayText).getText();
+    }
+    public void clickExtend(){
+        driver.findElement(extend).click();
+    }
+    public String getExtendText(){
+        return driver.findElement(extendText).getText();
+    }
+    public void clickCharger(){
+        driver.findElement(charger).click();
+    }
+    public String getChargerText(){
+        return driver.findElement(chargerText).getText();
+    }
+    public void clickCancel(){
+        driver.findElement(cancel).click();
+    }
+    public String getCancelText(){
+        return driver.findElement(cancelText).getText();
+    }
+    public void clickMkad(){
+        driver.findElement(mkad).click();
+    }
+    public String getMkadText(){
+        return driver.findElement(mkadText).getText();
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/HomePageTest.java
@@ -0,0 +1,122 @@
+package ru.yandex.practicum;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.After;
+import java.util.concurrent.TimeUnit;
+
+
+
+import static org.junit.Assert.*;
+
+
+public class HomePageTest {
+    private WebDriver driver;
+    @Before
+    public void startUp() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        driver.manage().window().maximize();
+
+
+    }
+    @After
+    public void teardown() {
+        // Закрой браузер
+        driver.quit();
+    }
+
+//    @Test
+//    public void waitForLoadHomePage() {
+//    }
+
+//    @Test
+//    public void clickPrice() {
+//    }
+
+    @Test
+    public void testPriceText() throws InterruptedException {
+        HomePage objHomePage = new HomePage(driver);
+        objHomePage.waitForLoadHomePage();
+//        Thread.sleep(5000);
+        objHomePage.clickHueta();
+        objHomePage.clickPrice();
+        String priceText = objHomePage.getPriceText();
+        assertEquals("Текст1 не корректный", priceText, "Сутки — 400 рублей. Оплата курьеру — наличными или картой.");
+
+
+    }
+    @Test
+    public void testManyText() {
+        HomePage objHomePage = new HomePage(driver);
+        objHomePage.waitForLoadHomePage();
+        objHomePage.clickHueta();
+        objHomePage.clickMany();
+        String manyText = objHomePage.getManyText();
+        assertEquals("Текст2 не корректный", manyText, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.");
+    }
+    @Test
+    public void testTimeText() {
+        HomePage objHomePage = new HomePage(driver);
+        objHomePage.waitForLoadHomePage();
+        objHomePage.clickHueta();
+        objHomePage.clickTime();
+        String timeText = objHomePage.getTimeText();
+        assertEquals("Текст3 не корректный", timeText, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.");
+    }
+    @Test
+    public void testTodayText() {
+        HomePage objHomePage = new HomePage(driver);
+        objHomePage.waitForLoadHomePage();
+        objHomePage.clickHueta();
+        objHomePage.clickToday();
+        String todayText = objHomePage.getTodayText();
+        assertEquals("Текст4 не корректный", todayText, "Только начиная с завтрашнего дня. Но скоро станем расторопнее.");
+    }
+    @Test
+    public void testExtendText() {
+        HomePage objHomePage = new HomePage(driver);
+        objHomePage.waitForLoadHomePage();
+        objHomePage.clickHueta();
+        objHomePage.clickExtend();
+        String extendText = objHomePage.getExtendText();
+        assertEquals("Текст5 не корректный", extendText, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.");
+    }
+    @Test
+    public void testChargerText() {
+        HomePage objHomePage = new HomePage(driver);
+        objHomePage.waitForLoadHomePage();
+        objHomePage.clickHueta();
+        objHomePage.clickCharger();
+        String chargerText = objHomePage.getChargerText();
+        assertEquals("Текст6 не корректный", chargerText, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.");
+    }
+    @Test
+    public void testCancelText() {
+        HomePage objHomePage = new HomePage(driver);
+        objHomePage.waitForLoadHomePage();
+        objHomePage.clickHueta();
+        objHomePage.clickCancel();
+        String cancelText = objHomePage.getCancelText();
+        assertEquals("Текст7 не корректный", cancelText, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.");
+    }
+    @Test
+    public void testMkadText() {
+        HomePage objHomePage = new HomePage(driver);
+        objHomePage.waitForLoadHomePage();
+        objHomePage.clickHueta();
+        objHomePage.clickMkad();
+        String mkadText = objHomePage.getMkadText();
+        assertEquals("Текст8 не корректный", mkadText, "Да, обязательно. Всем самокатов! И Москве, и Московской области.");
+    }
+}
